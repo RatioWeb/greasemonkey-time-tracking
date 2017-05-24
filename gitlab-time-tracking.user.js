74f03ee23ac97c207b8a230d9ec06aa2b34fb70b
javjaff <pj@infoweb.pl>
2017-05-24 12:18:43 +0200

    issue #3: free hours checkbox fix; error message appending

--- gitlab-time-tracking.user.js
+++ gitlab-time-tracking.user.js
@@ -13,40 +13,41 @@
 function createWidget() {
   var sum = aggregateSumOfTrackingComment();
   var budget = getEstimationTime();
-  
   var formatted_sum = (budget > 0) ? sum + "h/" + budget + 'h' : sum;
-  
-  
-  // Create time tracking widget
-  var timetracking = '<div class=\'block timetracking\'>' +
-  '<div class=\'title hide-collapsed\'>Time tracking (<span class="timetrack-data bold">' + formatted_sum +
-  '</span>)<a class="edit-link pull-right" href="#">Edit</a>' +
-  '</div>'
+
+  // Create time tracking widget.
+  var timetracking = '<div class=\'block timetracking\'>'
+  + '<div class=\'title hide-collapsed\'>Time tracking (<span class="timetrack-data bold">' + formatted_sum
+  + '</span>)<a class="edit-link pull-right" href="#">Edit</a>'
+  + '</div>'
+  + '<div id="timetracking-messages"></div>'
   + '<div class="selectbox hide-collapsed">'
   + '<div class="form-group"><div class="control-label">Date</div><div class="col-sm-12"><input placeholder="Date" class="datepicker" name="timetrack_date" /></div></div>'
   + '<div class="form-group"><div class="control-label">Hours</div><div class="col-sm-12"><input placeholder="Hours" value="0" type="number" min="0" step="1" name="timetrack_hours" /></div></div>'
   + '<div class="form-group"><div class="control-label">Minutes</div><div class="col-sm-12"><input placeholder="Minutes" value="0" type="number" min="0" max="60" step="15" name="timetrack_minutes" /></div></div>'
-  + '<div class="form-group"><div class="control-label">Description</div><div class="col-sm-12"><textarea name="timetrack_description" placeholder="Please enter your activity description here"></textarea></div></div>'
-  + '<div class="form-group"><div class="control-label">Free hours?</div><div><input type="checkbox" checked="false" /></div></div>'
+  + '<div class="form-group clearfix"><div class="control-label">Description</div><div class="col-sm-12"><textarea name="timetrack_description" placeholder="Please enter your activity description here"></textarea></div></div>'
+  + '<div class="form-group clearfix"><div class="pull-left">Free hours?</div><div class="control-label"><input id="timetracking-clock-free" type="checkbox"/></div></div>'
   + '<button name="create_timetrack" value="CreateTime" class="" type="submit">Update work log</button>'
   + '</div>'
-  '</div>';
-   var estimation = '<div class=\'block estimation\'>' +
-  '<div class=\'title hide-collapsed\'>Estimation (<span class="estimation-data bold">' + budget  +
-  'h</span>)<a class="edit-link pull-right" href="#">Edit</a>' +
-  '</div>'
+  + '</div>';
+
+// Create estimation widget.
+   var estimation = '<div class=\'block estimation\'>'
+  + '<div class=\'title hide-collapsed\'>Estimation (<span class="estimation-data bold">' + budget
+  + 'h</span>)<a class="edit-link pull-right" href="#">Edit</a>'
+  + '</div>'
   + '<div class="selectbox hide-collapsed">'
   + '<div class="form-group"><div class="control-label">Hours</div><div class="col-sm-12"><input placeholder="Hours" value="0" type="number" min="0" step="1" name="estimation_hours" /></div></div>'
   + '<div class="form-group"><div class="control-label">Minutes</div><div class="col-sm-12"><input placeholder="Minutes" value="0" type="number" min="0" max="60" step="15" name="estimation_minutes" /></div></div>'
-  + '<div class="form-group"><div class="control-label">Description</div><div class="col-sm-12"><textarea name="estimation_description" placeholder="Please enter your estimation description here"></textarea></div></div>'
+  + '<div class="form-group clearfix"><div class="control-label">Description</div><div class="col-sm-12"><textarea name="estimation_description" placeholder="Please enter your estimation description here"></textarea></div></div>'
   + '<button name="create_estimation" value="CreateEstimation" class="" type="submit">Update Time Estimation</button>'
   + '</div>'
-  '</div>';
+  + '</div>';
   $('form.inline-update').append(timetracking);
   $('form.inline-update').append(estimation);
-  
+
   var $dateField = $('input[name=timetrack_date]');
-  
+
   new Pikaday({
     field: $dateField.get(0),
     format: 'yyyy-mm-dd',
@@ -54,20 +55,21 @@ function createWidget() {
       dateField.val(dateFormat(new Date(dateText), 'yyyy-mm-dd'));
     }
   }).setDate(new Date($dateField.val()));
-  
 
   $('button[name=create_timetrack]').click(function () {
-    createTimeTrackingComment(':clock1:', '', 'timetrack');
+    if ($("#timetracking-clock-free").is(':checked')) {
+      createTimeTrackingComment(':clock1: :free:', '', 'timetrack');
+    } else {
+      createTimeTrackingComment(':clock1:', '', 'timetrack');
+    }
     return false;
   });
-  
+
   $('button[name=create_estimation]').click(function () {
     createTimeTrackingComment(':dart:', '', 'estimation');
     return false;
-  });  
+  });
 }
-
-
 
 /**
  * Create tracking comment.
@@ -81,10 +83,12 @@ function createTimeTrackingComment(emoji, additional, type) {
   var text = emoji + ' ' + additional;
   var time_is_entered = (hours + minutes > 0);
   var time_string = type === 'timetrack' ? "/spend " : "/estimate ";
-  
+
   if (!time_is_entered) {
-  } 
+    $("#timetracking-messages").addClass("cred").text("NOTICE: enter time data first!");
+  }
   else {
+    $("#timetracking-messages").removeClass("cred").text("");
     if (hours != 0) {
       time_string = time_string + hours + 'h';
       text = text + ' ' + hours + 'h';
@@ -103,17 +107,16 @@ function createTimeTrackingComment(emoji, additional, type) {
     $('textarea[name="note[note]"]').trigger('focus');
     $('textarea[name="note[note]"]').trigger('input');
     $('.btn-create.comment-btn').click();
-    
+
     setTimeout(
       function () {
         $('textarea[name="note[note]"]').val(time_string);
         $('textarea[name="note[note]"]').trigger('focus');
         $('textarea[name="note[note]"]').trigger('input');
-        $('.btn-create.comment-btn').click();    
+        $('.btn-create.comment-btn').click();
       },
       1500
-    )
-    
+    );
   }
 }
 
@@ -141,7 +144,6 @@ function aggregateSumOfTrackingComment() {
       );
 
     });
-
 
     return time.asHours();
 }
@@ -171,15 +173,13 @@ function getEstimationTime() {
 
     });
 
-
     return time.asHours();
 }
 
-
 $(
-  function () { 
-   createWidget(); 
-    
+  function () {
+   createWidget();
+
    // $('input.btn-save').on('click', )
   }
 );
